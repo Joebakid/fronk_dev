4f4a4446434a10c6f415960469482155214440e7
Laye <tlayefaah@gmail.com>
2024-01-29 13:31:16 +0100

    added favicon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -20,7 +20,9 @@ export default function Header() {
       <nav className="container mx-auto w-full bx-red-400 flex justify-center items-center gap-9 text-base">
         <ul ref={navRef} className="flex gap-10 bg-red-0">
           <li>
-            <a href="https://doggy.market/nfts/fronkcartel" target="_blank">Market</a>
+            <a href="https://doggy.market/nfts/devs" target="_blank">
+              Market
+            </a>
           </li>
           <li>
             <a href="https://twitter.com/FronkCartel">X</a>
