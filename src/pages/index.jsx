1e35eb9368d9e2d433e5caf682626c3a13e31804
Laye <tlayefaah@gmail.com>
2024-01-29 13:53:05 +0100

    added favicon

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -116,7 +116,7 @@ export default function Home() {
               // priority
               className="w-full lg:w-2/3 h-full object-contain"
             /> */}{" "}
-            <h1 className="text-8xl">.Dev</h1>
+            <h1 className="text-8xl">.Devs</h1>
           </div>
           <div className="flex flex-col justify-center mt-10 items-center px-4 ">
             <h2 className="text-xl text-center mt-0 mb-5">Search by number</h2>
